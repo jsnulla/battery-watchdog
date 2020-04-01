2d036e286595d045f5f676ef1b2b8a42dc29b786
Jason Jay Sumagdon Nulla <jasonnulla@gmail.com>
2020-04-02 07:48:50 +0800

    remove unused code

--- main.js
+++ main.js
@@ -8,31 +8,6 @@ let notification = null
 let notificationIsShown = false
 let notificationDismissed = false
 
-// function createWindow () {
-//   // Create the browser window.
-//   const win = new BrowserWindow({
-//     width: 800,
-//     height: 600,
-//     icon: path.join(__dirname, 'assets', 'images', 'AppIcon.iconset', 'icon_64x64.png'),
-//     webPreferences: {
-//       nodeIntegration: true
-//     }
-//   })
-//
-//   // and load the index.html of the app.
-//   win.loadFile('index.html')
-//
-//   // Open the DevTools.
-//   win.webContents.openDevTools()
-// }
-
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
-// app.whenReady().then(createWindow)
-
-// app.allowRendererProcessReuse = true
-
 // Quit when all windows are closed.
 app.on('window-all-closed', () => {
   // On macOS it is common for applications and their menu bar
@@ -41,14 +16,6 @@ app.on('window-all-closed', () => {
     app.quit()
   }
 })
-
-// app.on('activate', () => {
-//   // On macOS it's common to re-create a window in the app when the
-//   // dock icon is clicked and there are no other windows open.
-//   if (BrowserWindow.getAllWindows().length === 0) {
-//     createWindow()
-//   }
-// })
 
 async function getBatteryLevel() {
   await new Promise((res, rej) => {
